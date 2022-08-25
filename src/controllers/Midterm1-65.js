fd5515eacd1c23a8040b2079fd18523456cedbbd
Buraphon Saenthi <buraphons63@nu.ac.th>
2022-08-25 10:55:51 +0700

    Update Midterm1-65.js

--- src/controllers/Midterm1-65.js
+++ src/controllers/Midterm1-65.js
@@ -2,8 +2,8 @@ module.exports ={
 
     add1 (req,res){
         input = parseInt(JSON.stringify(req.body['input']))
-        console.log('input = '+ input)
-        output = input + 1
+        console.log('input = x '+ input)
+        output = input % 2
         res.send(output.toString())
     }
 
